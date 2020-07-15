9a454acbd595a96db004fc0ba555a88abe42de4f
Michelle Min <michellemin@michelles-air.lan>
2020-07-15 12:15:57 -0700

    updated formatting; saves data in trials

---
+++ fillers_info.js
@@ -0,0 +1,9 @@
+
+var filler_stimuli = [
+    {
+        choices: ["stimuli/bark.jpg", "stimuli/bark.jpg", "stimuli/bark.jpg", "stimuli/bark.jpg"],
+        audio: "stimuli/audio.mp3",
+        target: "stimuli/bark.jpg",
+        experiment_part: "filler",
+    }
+]
