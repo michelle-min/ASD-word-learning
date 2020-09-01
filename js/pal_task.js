344f4d91d5ae732790842ef0611340993bc80f9f
Michelle <michelleymin@gmail.com>
2020-08-31 23:42:43 -0700

    added functions; update PAL sorting task

--- js/pal_task.js
+++ js/pal_task.js
@@ -1,5 +1,4 @@
 /* 1. PAL: teaching blocks */
-/* just need to be able to change condition and order across participants!!! */
 
   /* Randomly choose a version for PAL stimuli condition */
   var version = jsPsych.randomization.sampleWithoutReplacement([1,2,3,4],1)[0];
@@ -15,34 +14,33 @@
     var freeSortImages2 = imagesA;
   }
 
-  /* Create timeline variables of video and audio for ambiguous and iconic pairs. */
-  var ambStimuli = [];
-  for (i = 0; i < 16; i++) {
-    var vidName = conditionStimuli[i];
-    var vidName = vidName.replace("videos/cond1_", "");
-    var vidName = vidName.replace(".mp4", "");
-    var vidName = vidName.split("_");
-    var vidPair = vidName[0];
-    var vidObjectNumber = vidName[1];
-    var vidObject = vidObjectNumber.slice(0, -1);
-    var vidNumber = vidObjectNumber.substr(vidObjectNumber.length - 1);
-    ambStimuli[i] = {
-      video: [conditionStimuli[i]], icoamb: vidPair, obj: vidObject, dir: vidNumber}, + "\n";
+  /* Timeline variable function */
+  function createTeachStimuli(timelineVar, rawStimuli, amb) {
+    for (i = 0; i < 16; i++) {
+      if (amb == true) {
+        index = i
+      } else {
+        index = i+16
+      }
+      var vidName = conditionStimuli[index];
+      var vidName = vidName.replace("videos/cond1_", "");
+      var vidName = vidName.replace(".mp4", "");
+      var vidName = vidName.split("_");
+      var vidPair = vidName[0];
+      var vidObjectNumber = vidName[1];
+      var vidObject = vidObjectNumber.slice(0, -1);
+      var vidNumber = vidObjectNumber.substr(vidObjectNumber.length - 1);
+      timelineVar[i] = {
+        video: [conditionStimuli[index]], icoamb: vidPair, obj: vidObject, dir: vidNumber}, + "\n";
+    }
+    return timelineVar;
   }
 
-  var icoStimuli = [];
-  for (i = 0; i < 16; i++) {
-    var vidName = conditionStimuli[i+16];
-    var vidName = vidName.replace("videos/cond1_", "");
-    var vidName = vidName.replace(".mp4", "");
-    var vidName = vidName.split("_");
-    var vidPair = vidName[0];
-    var vidObjectNumber = vidName[1];
-    var vidObject = vidObjectNumber.slice(0, -1);
-    var vidNumber = vidObjectNumber.substr(vidObjectNumber.length - 1);
-    icoStimuli[i] = {
-      video: [conditionStimuli[i+16]], icoamb: vidPair, obj: vidObject, dir: vidNumber}, + "\n";
-  }
+  /* Create timeline variables of video and audio for ambiguous and iconic pairs. */
+  var ambTeachStimuli = [];
+  var icoTeachStimuli = [];
+  createTeachStimuli(ambTeachStimuli, conditionStimuli, true)
+  createTeachStimuli(icoTeachStimuli, conditionStimuli, false)
 
   /* Video-keyboard trial for PAL task */
   var palVideokeyboard = {
@@ -67,34 +65,83 @@
   /* Procedure for ambiguous pairs teaching */
   var amb_teach_procedure = {
     timeline: [fixation, palVideokeyboard],
-    timeline_variables: ambStimuli,
+    timeline_variables: ambTeachStimuli,
     randomize_order: true
   };
 
   /* Procedure for iconic pairs teaching */
   var ico_teach_procedure = {
     timeline: [fixation, palVideokeyboard],
-    timeline_variables: icoStimuli,
+    timeline_variables: icoTeachStimuli,
     randomize_order: true
   };
 
 /* 2. PAL: free sort */
-  var amb_sort_procedure = {
-      type: 'free-sort',
-      stimuli: freeSortImages1,
-      data: {
-        version: version,
-        block: 'sort',
-        task: 'pal'
+
+  /* Timeline variable function */
+  function createSortStimuli(timelineVar, imageSet) {
+    for (i = 0; i < 8; i++) {
+      // id target symbol
+      var targetSymbol = imageSet[i+8]
+      // select random symbol
+      var randomNum = jsPsych.randomization.sampleWithoutReplacement([8,9,10,11,12,13,14,15],1)[0];
+      // if same as target symbol, repeat
+      while (randomNum = i+8) {
+        var randomNum = jsPsych.randomization.sampleWithoutReplacement([8,9,10,11,12,13,14,15],1)[0];
       }
+      // combime and shuffle symbol order
+      var icoChoices = [targetSymbol, imageSet[randomNum]];
+      var icoChoices = jsPsych.randomization.shuffle(icoChoices);
+      timelineVar[i] = {
+        image: imageSet[i], choices: icoChoices, target: targetSymbol, block: 'sort'}, + "\n";
+    }
+
+  }
+
+  /* Create timeline variable of object and 2 symbols for 8 trials; shuffle
+  symbol choice order within each trial */
+  var icoSortStimuli = [];
+  var ambSortStimuli = [];
+  createSortStimuli(icoSortStimuli, freeSortImages1)
+  createSortStimuli(ambSortStimuli, freeSortImages2)
+
+  /* Image-button trial */
+  var palImageButton = {
+    timeline: [
+      {
+        type: 'image-button-response',
+        stimulus: jsPsych.timelineVariable('image'),
+        choices: jsPsych.timelineVariable('choices'),
+        button_html: stimButton,
+        data: {
+          choices: jsPsych.timelineVariable('choices'),
+          image: jsPsych.timelineVariable('image'),
+          target: jsPsych.timelineVariable('target'),
+          block: jsPsych.timelineVariable('block'),
+          task: 'pal'
+        },
+        on_finish: function(data){
+          var acc = false;
+          var indexNumber = data.button_pressed;
+          if (data.target == data.choices[indexNumber]) {
+            acc = true
+          }
+          data.accuracy = acc;
+        }
+      }
+    ]
   };
 
+  /* Procedure for ambiguous pairs sorting */
   var ico_sort_procedure = {
-      type: 'free-sort',
-      stimuli: freeSortImages2,
-      data: {
-        version: version,
-        block: 'sort',
-        task: 'pal'
-      }
+    timeline: [fixation, palImageButton],
+    timeline_variables: icoSortStimuli,
+    randomize_order: true
+  };
+
+/* Procedure for iconic pairs sorting */
+  var ico_sort_procedure = {
+    timeline: [fixation, palImageButton],
+    timeline_variables: ambSortStimuli,
+    randomize_order: true
   };
